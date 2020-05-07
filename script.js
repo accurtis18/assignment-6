9aeeef0e5aaa0c4313c3d49fa50a8f04897fba4d
Alex <achriscurtis@gmail.com>
2020-05-07 17:26:19 -0500

    updated weather api to use 5 day forecast so can be based on city

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 $(document).ready(function(){
-    getCurrentForecast();
-    
+    var localCities = localStorage.getItem("cities");
+    var cities = [];
+    var city = '';
+    if(localCities !== null && localCities !== ""){
+        cities = JSON.parse(localCities);
+        city = cities[0];
+    } else{
+        city = 'Chicago';
+    }
 
-    function getCurrentForecast(){
+    getCurrentForecast(city);
+    
+    $('#search').on('click', function(){
+        city = $('#searchWeather').val();
+        getCurrentForecast(city);
+    });
+
+    function getCurrentForecast(city){
         var key = '9a89782c5d73128c63edf8b5a4c73c28';
-        var lat = '41.85';
-        var lon = '-87.65';
     
         $.ajax({
-            url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly&appid=' + key +'&units=imperial',
+            url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + key +'&units=imperial',
             method: 'GET'
         }).then(function(response){
             console.log(response);
-            var slash = response.timezone.search("/") + 1;
-            var length = response.timezone.length - slash;
-            var city = response.timezone.substr(parseInt(slash), parseInt(length));
-            $('.city').html(`${city} <img src='http://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png'/>`);
-            $('.temp').html(`Temp: ${Math.floor(response.current.temp)}&#176 and ${response.current.weather[0].description}`);
-            $('.humid').html(`Humidity: ${response.current.humidity}%`);
-            $('.wind').html(`Wind: ${response.current.wind_speed} MPH`);
-            $('.uv').html(`UV Index: ${response.current.uvi}`);
+            $('.city').html(`${response.city.name} <img src='http://openweathermap.org/img/wn/${response.list[5].weather[0].icon}@2x.png'/>`);
+            $('.temp').html(`Temp: ${Math.floor(response.list[5].main.temp)}&#176 and ${response.list[5].weather[0].description}`);
+            $('.humid').html(`Humidity: ${response.list[5].main.humidity}%`);
+            $('.wind').html(`Wind: ${response.list[5].wind.speed} MPH`);
+            // $('.uv').html(`UV Index: ${response.current.uvi}`);
             getFutureForecast(response);
         });        
     };
 
     function getFutureForecast(response){
         var futureDays = 0;
+        var listItem = 5;
+        $('.future').html('');
 
         while(futureDays < 5){
             $('.future').append(`<div class="card">
@@ -33,12 +44,13 @@ $(document).ready(function(){
                 ${moment().add((futureDays+1), 'days').format('l')}
             </div>
             <div class="card-body">
-                <p><img src='http://openweathermap.org/img/wn/${response.daily[futureDays].weather[0].icon}@2x.png'/></p>
-                <p>Temp: ${Math.floor(response.daily[futureDays].temp.day)}&#176</p>
-                <p>Humidity: ${response.daily[futureDays].humidity}%</p>
+                <p><img src='http://openweathermap.org/img/wn/${response.list[listItem].weather[0].icon}@2x.png'/></p>
+                <p>Temp: ${Math.floor(response.list[listItem].main.temp)}&#176</p>
+                <p>Humidity: ${response.list[listItem].main.humidity}%</p>
             </div>
         </div>`);
         futureDays++;
+        listItem += 8;
         };
     }
 
