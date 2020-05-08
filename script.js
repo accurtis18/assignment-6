1d0e9dee4e4da1ff82d21d57e9b9ea17d7eca55c
Alex <achriscurtis@gmail.com>
2020-05-08 17:58:56 -0500

    updated weather apis to use two, so we can get uvi

--- script.js
+++ script.js
@@ -2,77 +2,113 @@ $(document).ready(function(){
     var localCities = localStorage.getItem("cities");
     var cities = [];
     var city = '';
+    var fullCast = true;
     if(localCities !== null && localCities !== ""){
         cities = JSON.parse(localCities);
         city = cities[0];
-        console.log(city);
-        getCurrentForecast(city);
+        getCurrentForecast(city, fullCast);
         writeCityHistory(cities);
     } else{
             $.ajax({
                 url: 'http://free.ipwhois.io/json/',
                 method: 'GET'
             }).then(function(response){
-              console.log(response);
-              getCurrentForecast(response.city)
+              getCurrentForecast(response.city, fullCast)
             });
     }
-    
-    $('#search').on('click', function(){
+
+    function search(city){
         city = $('#searchWeather').val();
         cities.unshift(city.trim());
         cities.splice(5);
         localStorage.setItem("cities", JSON.stringify(cities));
-        getCurrentForecast(city.trim());
+        getCurrentForecast(city.trim(), fullCast);
         $('#searchWeather').val("");
         writeCityHistory(cities);
+    }
+    
+    $('#searchWeather').keypress(function (e) {
+        if (e.which == 13) {
+            city = $('#searchWeather').val();
+            search(city);
+          return false;
+        }
+      });
+
+    $('#search').on('click', function(){
+        city = $('#searchWeather').val();
+        search(city);
     });
+
+    $('.history').on("click", '#cityHis', function(){
+            city = $(this).closest('.his').val();
+            console.log(city);
+            search(city);
+    });
+
+ 
 
     //error response for incorrect cities/make sure it doesn't push or delete if it already has
     //add celsius toggle
-    //need second call to collect todays UV index
-    function getCurrentForecast(city){
+    function getCurrentForecast(city, full){
         var key = '9a89782c5d73128c63edf8b5a4c73c28';
     
         $.ajax({
-            url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + key +'&units=imperial',
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key +'&units=imperial',
             method: 'GET'
         }).then(function(response){
             console.log(response);
-            $('.city').html(`${response.city.name} <img src='http://openweathermap.org/img/wn/${response.list[5].weather[0].icon}@2x.png'/>`);
-            $('.temp').html(`Temp: ${Math.floor(response.list[5].main.temp)}&#176 and ${response.list[5].weather[0].description}`);
-            $('.humid').html(`Humidity: ${response.list[5].main.humidity}%`);
-            $('.wind').html(`Wind: ${response.list[5].wind.speed} MPH`);
-            // $('.uv').html(`UV Index: ${response.current.uvi}`);
-            getFutureForecast(response);
+            if(full){
+                $('.city').html(`${response.name} <img src='http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png'/>`);
+                $('.temp').html(`Temp: ${Math.floor(response.main.temp)}&#176 and ${response.weather[0].description}`);
+                $('.humid').html(`Humidity: ${response.main.humidity}%`);
+                $('.wind').html(`Wind: ${response.wind.speed} MPH`);
+                getFutureForecast(response);
+            } else{
+                // return response.main.temp;
+            }
+        }).catch(function(error){
+            alert("We couldn't find that city. Please try again.")
+            cities.shift();
+            localStorage.setItem("cities", JSON.stringify(cities));
+            writeCityHistory(cities);
         });        
     };
 
     function getFutureForecast(response){
+        var key = '9a89782c5d73128c63edf8b5a4c73c28';
         var futureDays = 0;
-        var listItem = 5;
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
         $('.future').html('');
 
-        while(futureDays < 5){
-            $('.future').append(`<div class="card">
-            <div class="card-header">
-                ${moment().add((futureDays+1), 'days').format('l')}
-            </div>
-            <div class="card-body">
-                <p><img class="futureImg" src='http://openweathermap.org/img/wn/${response.list[listItem].weather[0].icon}@2x.png'/></p>
-                <p>Temp: ${Math.floor(response.list[listItem].main.temp)}&#176</p>
-                <p>Humidity: ${response.list[listItem].main.humidity}%</p>
-            </div>
-        </div>`);
-        futureDays++;
-        listItem += 8;
-        };
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' +lat + '&lon=' + lon + '&exclude=hourly&appid=' + key +'&units=imperial',
+            method: 'GET'
+        }).then(function(response){
+            console.log(response);
+            while(futureDays < 5){
+                $('.future').append(`<div class="card">
+                <div class="card-header">
+                    ${moment().add((futureDays+1), 'days').format('l')}
+                </div>
+                <div class="card-body">
+                    <p><img class="futureImg" src='http://openweathermap.org/img/wn/${response.daily[futureDays].weather[0].icon}@2x.png'/></p>
+                    <p>Temp: ${Math.floor(response.daily[futureDays].temp.day)}&#176</p>
+                    <p>Humidity: ${response.daily[futureDays].humidity}%</p>
+                </div>
+            </div>`);
+            futureDays++;
+            };
+            $('.uv').html(`UV Index: ${response.current.uvi}`);
+        });
+
     }
 
     function writeCityHistory(cities){
         $('.history').html("");
         for(city of cities){
-            $('.history').append(`<li class="list-group-item"> <a id="${city}" href="#">${city}</a></li>`);
+            $('.history').append(`<li class="list-group-item"> <a class="his" id="cityHis" href="#">${city}</a></li>`);
         }
     }
 
